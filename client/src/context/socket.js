0952799b68f5543e490ed194301c65a041401019
Syed Meesum Raza NAqvi <70362501+meesum70110@users.noreply.github.com>
2024-10-03 02:18:57 +0500

    Update socket.js
    Linking backend service on render

--- client/src/context/socket.js
+++ client/src/context/socket.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import {io} from 'socket.io-client';
 
-export const socket =  io('/',{
+export const socket =  io('https://workspacereservation-backend.onrender.com/',{
     reconnection: true,
     cors: {
         origin: '*'
