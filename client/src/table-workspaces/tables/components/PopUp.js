78c4a705e5a7a06a549a926044edb8862a3f983b
Syed Meesum Raza NAqvi <70362501+meesum70110@users.noreply.github.com>
2024-11-25 16:56:20 +0500

    Update PopUp.js

--- client/src/table-workspaces/tables/components/PopUp.js
+++ client/src/table-workspaces/tables/components/PopUp.js
@@ -1,53 +1,36 @@
-// Importing necessary libraries and CSS
-import React from 'react'; // Importing React library
-import moment from 'moment'; // Importing moment for date/time formatting
-import styles from './css/PopUp.module.css'; // Importing custom CSS module for styling
+import React from 'react';
+import moment from 'moment';
+import styles from './css/PopUp.module.css';
 
-// Defining the PopUp component
 const PopUp = ({ handlePopUp, handleReservation, from, to, first_name, last_name }) => {
 
-    // Function to handle user confirmation actions
+    // Handle button click for confirmation
     const handleClick = (confirmation) => {
-        if (confirmation) { // If the user confirms the reservation
-            handleReservation(); // Call the reservation handler
-        } else { // If the user declines
-            handlePopUp(false); // Close the popup
+        if (confirmation) {
+            handleReservation();  // Proceed with reservation if confirmed
+        } else {
+            handlePopUp(false);  // Close popup if not confirmed
         }
     };
 
-    // Returning the JSX structure for the popup
     return (
-        <div className={styles.PopUpBackground}> {/* Background overlay for the popup */}
-            <div className={styles.PopUp}> {/* Main popup container */}
-                <div className={styles.Title}> {/* Section for the popup title */}
-                    <h4>CONFIRM TABLE RESERVATION?</h4> {/* Title message */}
+        <div className={styles.PopUpBackground}>
+            <div className={styles.PopUp}>
+                <div className={styles.Title}>
+                    <h4>CONFIRM TABLE RESERVATION?</h4>
                 </div>
-                <div className={styles.ConfirmationInfo}> {/* Section displaying reservation details */}
-                    <p>
-                        FROM: <span>{moment(from).format('MMMM Do YYYY, h:mm:ss a')}</span> {/* Displaying formatted start time */}
-                    </p>
-                    <p>
-                        TO: <span>{moment(to).format('MMMM Do YYYY, h:mm:ss a')}</span> {/* Displaying formatted end time */}
-                    </p>
-                    <p>
-                        FIRST NAME: <span>{first_name}</span> {/* Displaying user's first name */}
-                    </p>
-                    <p>
-                        LAST NAME: <span>{last_name}</span> {/* Displaying user's last name */}
-                    </p>
+                <div className={styles.ConfirmationInfo}>
+                    <p>FROM: <span>{moment(from).format('MMMM Do YYYY, h:mm:ss a')}</span></p>
+                    <p>TO: <span>{moment(to).format('MMMM Do YYYY, h:mm:ss a')}</span></p>
+                    <p>FIRST NAME: <span>{first_name}</span></p>
+                    <p>LAST NAME: <span>{last_name}</span></p>
                 </div>
-                <div className={styles.Buttons}> {/* Section for action buttons */}
-                    <div 
-                        className={styles.Button} 
-                        id={styles.ButtonGreen} 
-                        onClick={() => handleClick(true)}> {/* Confirm button */}
-                        <h4>YES</h4> {/* Text on the confirm button */}
+                <div className={styles.Buttons}>
+                    <div className={styles.Button} id={styles.ButtonGreen} onClick={() => handleClick(true)}>
+                        <h4>YES</h4>
                     </div>
-                    <div 
-                        className={styles.Button} 
-                        id={styles.ButtonRed} 
-                        onClick={() => handleClick(false)}> {/* Decline button */}
-                        <h4>NO</h4> {/* Text on the decline button */}
+                    <div className={styles.Button} id={styles.ButtonRed} onClick={() => handleClick(false)}>
+                        <h4>NO</h4>
                     </div>
                 </div>
             </div>
@@ -55,4 +38,5 @@ const PopUp = ({ handlePopUp, handleReservation, from, to, first_name, last_name
     );
 };
 
-export default PopUp; // Exporting the PopUp component as default
+export default PopUp;
+
