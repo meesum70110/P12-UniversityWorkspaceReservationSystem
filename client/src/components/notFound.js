0e5a57fd7b44d12de8835e1aaa9a550f2697bbec
Syed Meesum Raza NAqvi <70362501+meesum70110@users.noreply.github.com>
2024-11-25 10:19:33 +0500

    Update notFound.js

--- client/src/components/notFound.js
+++ client/src/components/notFound.js
@@ -1,26 +1,33 @@
-import {Link} from 'react-router-dom';
+// Importing the Link component from React Router for navigation
+import { Link } from 'react-router-dom';
 
-import '../styles/login.css'
-import '../styles/notfound.css'
+// Importing CSS styles specific to the login and not found pages
+import '../styles/login.css'; // Styling for the login page
+import '../styles/notfound.css'; // Styling for the not found page
+
+// Importing animation library for adding bounce animation
 import 'animate.css';
 
-import no_page_icon from './Images/Nan/not-found-img.png'
+// Importing a placeholder image to display when the page is not found
+import no_page_icon from './Images/Nan/not-found-img.png'; // Icon for the "Not Found" page
 
-const NotFound = (prop)=>{
-
-
+// Functional component definition for the NotFound page
+const NotFound = (prop) => {
     return (   
         <div className="notfound">
-            <div className="top-bar-login">
-            </div>
+            {/* Top bar for consistent layout styling */}
+            <div className="top-bar-login"></div> 
 
+            {/* Main content wrapper with bounce animation */}
             <div className="not-found-wrapper animate__animated animate__bounce">
+                {/* Displaying an image to indicate the page is not found */}
                 <img src={no_page_icon} alt="Record None" />
+                {/* Link to navigate back to the login page */}
                 <Link to="/login">Go Back</Link>
             </div>
-
         </div>
-    )
-}
+    );
+};
 
-export default NotFound
+// Exporting the NotFound component as the default export
+export default NotFound;
