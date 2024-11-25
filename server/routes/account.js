83a95d821e4651c780a9de31e5d4085be7233589
Syed Meesum Raza NAqvi <70362501+meesum70110@users.noreply.github.com>
2024-11-25 12:25:06 +0500

    Update account.js

--- server/routes/account.js
+++ server/routes/account.js
@@ -1,43 +1,49 @@
+// Importing required dependencies and controller functions
 const express = require('express');
 const {
-    getProfile,
-    searchUsers,
-    updatePhoto,
-    updatePassword,
-    updateInfo,
-    getProfileAdmin,
-    updateSensitive,
-    deleteUser
-} = require('../controllers/accountController')
+    getProfile, // Controller to get the user profile
+    searchUsers, // Controller to search for users
+    updatePhoto, // Controller to update user photo
+    updatePassword, // Controller to update user password
+    updateInfo, // Controller to update user information
+    getProfileAdmin, // Controller to get user profile by admin
+    updateSensitive, // Controller to update sensitive company-specific information
+    deleteUser // Controller to delete a user
+} = require('../controllers/accountController');
 
+// Creating a new Express router instance
 const router = express.Router();
+
+// Importing the middleware to authenticate requests
 const authenticateRequest = require('../middleware/authorize');
 
-// JSON WEB TOKEN validation. If valid only then subsequent routes are invoked and access to controller functions is granted.
+// Applying the authentication middleware to validate the JSON Web Token (JWT)
+// If the token is valid, it allows access to the subsequent routes and controller functions
 router.use(authenticateRequest);
 
-// GET user profile
+// Defining the route to GET the user profile
 router.get('/', getProfile);
 
-// GET all employee profiles
+// Defining the route to POST and search users by certain criteria
 router.post('/', searchUsers);
 
-// Update user photo
+// Defining the route to PATCH and update the user photo
 router.patch('/photo', updatePhoto);
 
-// Update user password
+// Defining the route to PATCH and update the user password
 router.patch('/password', updatePassword);
 
-// Update user info
+// Defining the route to PATCH and update general user info
 router.patch('/info', updateInfo);
 
-// Retrieve user profile requested by admin
+// Defining the route to GET the user profile requested by an admin
 router.post('/profile', getProfileAdmin);
 
-// UPDATE user company specific info
+// Defining the route to PATCH and update company-specific sensitive information
 router.patch('/sensitive', updateSensitive);
 
-// Delete a user
+// Defining the route to DELETE a user by their ID
 router.delete('/:id', deleteUser);
 
-module.exports = router
+// Exporting the router to be used in the main app
+module.exports = router;
