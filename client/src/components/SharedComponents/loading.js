da79a22273631af60a95b6d85d5ade4ea5556e48
Syed Meesum Raza NAqvi <70362501+meesum70110@users.noreply.github.com>
2024-11-25 10:36:26 +0500

    Update loading.js

--- client/src/components/SharedComponents/loading.js
+++ client/src/components/SharedComponents/loading.js
@@ -1,15 +1,21 @@
+// Importing the CSS styles for the loading icon component
 import './sharedComponents.css';
 
-const LoadingIcon = (prop)=>{
+// Functional component definition for the Loading Icon
+const LoadingIcon = (prop) => {
 
-    return (   
+    return (
         <span>
-            <div className="loadingio-spinner-rolling-2by998twmg8"><div className="ldio-yzaezf3dcmj">
-            <div></div>
-            </div></div>
+            {/* Main wrapper for the loading spinner */}
+            <div className="loadingio-spinner-rolling-2by998twmg8">
+                <div className="ldio-yzaezf3dcmj">
+                    {/* Inner div for the animated rolling spinner */}
+                    <div></div>
+                </div>
+            </div>
         </span>
-    )
+    );
+};
 
-}
-
-export default LoadingIcon
+// Exporting the LoadingIcon component as the default export
+export default LoadingIcon;
