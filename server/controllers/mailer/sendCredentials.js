c1fac4e8083478fee18433e0e49d09692b3a3f88
Syed Meesum Raza NAqvi <70362501+meesum70110@users.noreply.github.com>
2024-11-25 12:28:28 +0500

    Update sendCredentials.js

--- server/controllers/mailer/sendCredentials.js
+++ server/controllers/mailer/sendCredentials.js
@@ -1,41 +1,41 @@
+// Importing nodemailer module for sending emails
 const nodeMailer = require('nodemailer');
 
-
+// Function to send credentials via email
 const sendCredentials = async (email, password) => {
-    
-    var nodemailer = require('nodemailer');
 
-    var transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: 'hrsystemseproject@gmail.com',
-        pass: 'mysl ecpg bdoj kgbf'
-      }
+    // Creating a transporter object to configure the email service
+    var transporter = nodeMailer.createTransport({
+        service: 'gmail', // Using Gmail's email service
+        auth: {
+            user: 'hrsystemseproject@gmail.com', // Gmail account used for sending the email
+            pass: 'mysl ecpg bdoj kgbf' // The app password used for Gmail account authentication (Note: It's better to use environment variables for sensitive information)
+        }
     });
 
+    // HTML content for the email body, which contains the email and password
     const html = '<h1>Welcome Onboard!</h1> <h2>Below are your Workspace app login credentials</h2> <p>Email: '
-    + email + '<br>Password: ' + password + '</p>';
+        + email + '<br>Password: ' + password + '</p>';
     
+    // Setting up the mail options (email subject, receiver, sender, and body content)
     var mailOptions = {
-        from: 'hrsystemseproject@gmail.com',
-        to : email,
-        subject: 'Account Credential HR System',
-        html: html
+        from: 'hrsystemseproject@gmail.com', // Sender's email address
+        to: email, // Recipient's email address (provided in the function)
+        subject: 'Account Credential HR System', // Subject of the email
+        html: html // HTML body content for the email
     };
-    
-    transporter.sendMail(mailOptions, function(error, info){
-      if (error) {
-        console.log(error);
-      } else {
-        console.log('Email sent: ' + info.response);
-      }
+
+    // Sending the email using the transporter and mail options
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            console.log(error); // Log the error if email sending fails
+        } else {
+            console.log('Email sent: ' + info.response); // Log the response if email is successfully sent
+        }
     });
-
-
-
-
 }
 
+// Exporting the function for use in other parts of the application
 module.exports = {
     sendCredentials
-}
+};
