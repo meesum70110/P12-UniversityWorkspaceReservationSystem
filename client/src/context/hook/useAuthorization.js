0a9ee5477d4f735a236e5a1bf4867d6f133b4f66
Syed Meesum Raza NAqvi <70362501+meesum70110@users.noreply.github.com>
2024-11-25 11:39:51 +0500

    Update useAuthorization.js

--- client/src/context/hook/useAuthorization.js
+++ client/src/context/hook/useAuthorization.js
@@ -1,11 +1,19 @@
-import {Authorize} from "../authorization";
-import {useContext} from "react";
+// Importing the `Authorize` context from the authorization module.
+import { Authorize } from "../authorization";
+// Importing the `useContext` hook from React for consuming context.
+import { useContext } from "react";
 
+// Defining a custom hook for accessing the authorization context.
 export const useAuthorize = () => {
+    // Using the `useContext` hook to access the current value of the `Authorize` context.
     const context = useContext(Authorize);
 
+    // Checking if the context is being used outside of an AuthorizationProvider.
     if (!context) {
+        // Throwing an error if the context is not wrapped in an `AuthorizationProvider`.
         throw Error('useAuthorize must be used inside an AuthorizationProvider');
     }
+
+    // Returning the context for use in other components.
     return context;
-}
+};
