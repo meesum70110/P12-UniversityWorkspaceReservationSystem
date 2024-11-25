cf76a5dcebb2af331372f4537b0c7031259b9659
Syed Meesum Raza NAqvi <70362501+meesum70110@users.noreply.github.com>
2024-11-25 12:20:00 +0500

    Update booking.js

--- server/models/booking.js
+++ server/models/booking.js
@@ -1,40 +1,46 @@
+// Importing the Mongoose library for database schema and model creation
 const mongoose = require('mongoose'); 
+
+// Extracting Schema from Mongoose to define data structure
 const Schema = mongoose.Schema;
+
+// Importing Moment.js for date manipulation (not used here but useful for date formatting)
 const moment = require('moment'); 
 
+// Defining the Booking schema to represent booking documents in the database
 const bookingSchema = new Schema({
     workspace: { 
-        type: Schema.Types.ObjectId, 
-        ref: 'Workspace', 
-        required: true 
+        type: Schema.Types.ObjectId, // Reference to the associated Workspace document
+        ref: 'Workspace', // Links this field to the Workspace model
+        required: true // Ensures that this field is mandatory
     },
     room: { 
-        type: String, 
-        required: true 
+        type: String, // Stores the name or identifier of the room
+        required: true // Ensures this field must be provided
     },
     tableNumber: { 
-        type: String, 
-        required: true 
+        type: String, // Stores the identifier of the table being booked
+        required: true // Makes this field mandatory
     },
     firstName: {
-        type: String,
-        required: true
+        type: String, // Stores the first name of the person making the booking
+        required: true // Makes this field mandatory
     },
     email: {
-        type: String,
-        required: true
+        type: String, // Stores the email address of the person making the booking
+        required: true // Ensures this field must be provided
     },
     date: { 
-        type: Date, 
-        required: true 
+        type: Date, // Stores the date of the booking
+        required: true // Ensures the date field is mandatory
     },
     timeSlot: { 
-        type: String, 
-        required: true 
+        type: String, // Stores the time slot for the booking
+        required: true // Makes this field mandatory
     }
-}, { timestamps: true });
+}, { 
+    timestamps: true // Automatically adds createdAt and updatedAt fields
+});
 
+// Exporting the Booking model for use in other parts of the application
 module.exports = mongoose.model('Booking', bookingSchema);
-
-
-
