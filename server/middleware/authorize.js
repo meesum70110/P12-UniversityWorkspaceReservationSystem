5cf8616f8efb94a1b5c2cd061d14ee032b1922b4
Syed Meesum Raza NAqvi <70362501+meesum70110@users.noreply.github.com>
2024-11-25 12:27:47 +0500

    Update authorize.js

--- server/middleware/authorize.js
+++ server/middleware/authorize.js
@@ -1,34 +1,43 @@
-const User = require('../models/userData');
-const mongoose = require('mongoose');
+// Importing required dependencies
+const User = require('../models/userData'); // Importing the User model to interact with the user data
+const mongoose = require('mongoose'); // Importing mongoose (though it's not explicitly used here)
+const jwt = require('jsonwebtoken'); // Importing the JSON Web Token library for token verification
 
-const jwt = require('jsonwebtoken');
-
+// Middleware function to authenticate requests
 const authenticateRequest = async (req, res, next) => {
 
-    const {authorization} = req.headers;
+    // Extracting the 'authorization' field from request headers
+    const { authorization } = req.headers;
 
+    // Initializing error fields and list to send in case of failure
     let errorFields = [];
     let errorList = {};
 
+    // If there's no authorization header, return an error
     if (!authorization) 
-        return res.status(401).json({error : 'Authorization data not found', errorList, errorFields});
+        return res.status(401).json({ error: 'Authorization data not found', errorList, errorFields });
 
-    // JSON WEB TOKENS structure is as follows 'Bearer djef3iojfoi4fn4jfo4jfofj4ifjfj3f3fn3ofj3'.
-    // String with a single space with second substring being the actual token.
+    // Try block for processing the JWT token
+    try {
+        // The authorization header is expected to have the format: 'Bearer <token>'
+        // So, we split it into an array and extract the token
+        let jsonToken = authorization.split(' ')[1]; // Extracting token from the header
 
-    try{
-        let jsonToken = authorization.split(' ')[1];
-        const {email} = jwt.verify(jsonToken, process.env.SYM_KEY);
+        // Verifying the JWT token using the symmetric key from environment variables
+        const { email } = jwt.verify(jsonToken, process.env.SYM_KEY);
 
-        // Now attaching this _id to req object so that it is avaiable for subsequent API calls
-        req.email = await User.findOne({email}).select('email');
+        // If the token is valid, we fetch the user associated with the email from the database
+        // We only need the email field to verify the user
+        req.email = await User.findOne({ email }).select('email');
 
+        // If user exists, proceed to the next middleware/controller function
         next();
-    }
-    catch (error){
+    } catch (error) {
+        // If an error occurs (invalid token, expired token, etc.), return a 401 Unauthorized response
         console.log(error);
-        res.status(401).json({error: 'Access denied', errorList, errorFields});
+        res.status(401).json({ error: 'Access denied', errorList, errorFields });
     }
 }
 
-module.exports = authenticateRequest
+// Exporting the middleware to be used in the routes
+module.exports = authenticateRequest;
