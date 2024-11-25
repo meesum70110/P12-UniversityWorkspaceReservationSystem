65104517b9be207bdf97f7e6d6c7ac07f04777c7
Syed Meesum Raza NAqvi <70362501+meesum70110@users.noreply.github.com>
2024-11-25 12:25:51 +0500

    Update login.js

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,11 +1,14 @@
+// Importing required dependencies and controller functions
 const express = require('express');
 const {
-    loginRequest,
-} = require('../controllers/loginController')
+    loginRequest, // Controller to handle the login request
+} = require('../controllers/loginController');
 
+// Creating a new Express router instance
 const router = express.Router();
 
-// Login route
+// Defining the login route, which handles POST requests for user login
 router.post('/', loginRequest);
 
-module.exports = router
+// Exporting the router to be used in the main app
+module.exports = router;
