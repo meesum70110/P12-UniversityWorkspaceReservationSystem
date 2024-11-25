89db74edc056533454852b5784177cd3fcf363c8
Syed Meesum Raza NAqvi <70362501+meesum70110@users.noreply.github.com>
2024-11-25 10:37:16 +0500

    Update menuOptions.js

--- client/src/components/SharedComponents/menuOptions.js
+++ client/src/components/SharedComponents/menuOptions.js
@@ -1,45 +1,47 @@
+// Exporting an array of menu options for administrators
 export const MenuOptionsAdmin = [
     {
-        title : 'Home',
-        path : '/'
-    },  
+        title: 'Home', // Title displayed in the navigation menu
+        path: '/' // Path to the home page
+    },
     {
-        title : 'My Account',
-        path : '/account'
-    },     
+        title: 'My Account', // Title displayed for account management
+        path: '/account' // Path to the "My Account" page
+    },
     { 
-        title : 'User Records',
-        path : '/record-manage'
+        title: 'User Records', // Title displayed for managing user records
+        path: '/record-manage' // Path to the user records management page
     },
     {
-        title : 'Add User',
-        path : '/create-account'
-    },   
-    {
-        title : 'Add a WorkSpace',
-        path : '/workspaces'
+        title: 'Add User', // Title displayed for adding new users
+        path: '/create-account' // Path to the user creation page
     },
     {
-        title : 'FAQS',
-        path : '/faqs-manage'
-    }  
-]
+        title: 'Add a WorkSpace', // Title displayed for workspace management
+        path: '/workspaces' // Path to the workspace management page
+    },
+    {
+        title: 'FAQS', // Title displayed for managing FAQs
+        path: '/faqs-manage' // Path to the FAQ management page
+    }
+];
 
+// Exporting an array of menu options for employees
 export const MenuOptionsEmployee = [
     {
-        title : 'Home',
-        path : '/user'
-    }, 
+        title: 'Home', // Title displayed in the navigation menu
+        path: '/user' // Path to the employee home page
+    },
     {
-        title : 'My Account',
-        path : '/account'
-    },    
-    { 
-        title : 'View workspaces',
-        path : '/workspaces'
+        title: 'My Account', // Title displayed for account management
+        path: '/account' // Path to the "My Account" page
     },
     { 
-        title : 'FAQS',
-        path : '/faqs'
+        title: 'View workspaces', // Title displayed for viewing workspaces
+        path: '/workspaces' // Path to the workspace viewing page
+    },
+    { 
+        title: 'FAQS', // Title displayed for accessing FAQs
+        path: '/faqs' // Path to the FAQ page
     }
-]
+];
