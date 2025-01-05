c1c86ea06da6f006316f7818513c7801d68cfdfe
Syed Meesum Raza NAqvi <70362501+meesum70110@users.noreply.github.com>
2025-01-05 15:38:23 +0500

    Update admin.js

--- client/src/components/admin.js
+++ client/src/components/admin.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'; // Link component for navigation
 import { useState } from 'react'; // useState hook for managing component state (not used in this file, but imported)
 
 // Importing styles for the admin homepage
-import '../styles/homepage.css'; // CSS styles specific to the homepage
+import '../styles/adminHomepage.css'; // CSS styles specific to the homepage
 import 'animate.css'; // Animation library for visual effects
 
 // Importing images for the admin options section
@@ -22,53 +22,52 @@ import CustomFooter from './SharedComponents/customFooter'; // Footer component
 // Functional component definition for the Admin homepage
 const Admin = (prop) => {
     return (   
-        <div className="homepage">
-            {/* Main Navigation Bar */}
-            <NavMenu isAdmin={true} isHome={true} pagePath="/" /> {/* NavMenu for the Admin homepage */}
+    <div className="homepage">
+      {/* Main Nav Bar */}
+      <NavMenu isAdmin={true} isHome={true} pagePath="/" />
 
-            {/* Cover Section */}
-            <div className="cover-wrapper">
-                {/* Displaying the cover image */}
-                <img src={analytics_icon} alt="HR Cover" />
-            </div>
+      {/* Main Content Section */}
+      <div className="content-wrapper">
+        <h2>Welcome to Admin Dashboard</h2>
+      </div>
 
-            {/* Admin Options Section */}
-            <section className="scrolling-wrapper animate__animated animate__fadeInUp">
-                {/* Tile for "My Account" */}
-                <div className="tile-emp">
-                    <img src={account_icon} alt="My Account" /> {/* Image for "My Account" */}
-                    <Link to="/account" className="form_btn">My Account</Link> {/* Link to the "My Account" page */}
-                </div>
-
-                {/* Tile for "User Records" */}
-                <div className="tile-admin">
-                    <img src={records_icon} alt="Employee Records" /> {/* Image for "User Records" */}
-                    <Link to="/record-manage" className="form_btn">User Records</Link> {/* Link to the "User Records" page */}
-                </div>
-
-                {/* Tile for "Add User" */}
-                <div className="tile-admin">
-                    <img src={AddUser_icon} alt="Add Employee" /> {/* Image for "Add User" */}
-                    <Link to="/create-account" className="form_btn">Add User</Link> {/* Link to the "Add User" page */}
-                </div>
-                {/* Tile for "Workspaces" */} 
-                <div className="tile-emp">
-                    <img src={signup_icon} alt="Surveys"/>
-                    <Link to="/workspaces" className="form_btn">Book WorkSpaces</Link>
-                </div>
-
-                {/* Tile for "FAQs" */}
-                <div className="tile-admin">
-                    <img src={faqs_icon} alt="FAQS" /> {/* Image for "FAQs" */}
-                    <Link to="/faqs-manage" className="form_btn">FAQS</Link> {/* Link to the "FAQs" page */}
-                </div>
-            </section>
-
-            {/* Footer Section */}
-            <CustomFooter /> {/* Custom footer component */}
-        </div>
-    );
+      {/* Admin Options Moved to Bottom */}
+      <div className="options-container">
+        <section className="scrolling-wrapper animate__animated animate__fadeInUp">
+          <div className="tile-emp">
+            <img src={account_icon} alt="My Account" />
+            <Link to="/account" className="form_btn">
+              My Account
+            </Link>
+          </div>
+          <div className="tile-admin">
+            <img src={records_icon} alt="Employee Records" />
+            <Link to="/record-manage" className="form_btn">
+              User Records
+            </Link>
+          </div>
+          <div className="tile-admin">
+            <img src={signup_icon} alt="Add Employee" />
+            <Link to="/create-account" className="form_btn">
+              Add User
+            </Link>
+          </div>
+          <div className="tile-emp">
+            <img src={feedback_icon} alt="Surveys" />
+            <Link to="/workspaces" className="form_btn">
+              Book WorkSpaces
+            </Link>
+          </div>
+          <div className="tile-admin">
+            <img src={faqs_icon} alt="FAQS" />
+            <Link to="/faqs-manage" className="form_btn">
+              Create FAQS
+            </Link>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
 };
 
-// Exporting the Admin component as the default export
 export default Admin;
